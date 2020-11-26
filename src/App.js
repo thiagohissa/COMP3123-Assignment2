f8294f81ad0b593ac2dad269427e9982855fea39
thiago <thiagohissa@gmail.com>
2020-11-26 17:36:21 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Forecast from "./Forecast";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import API from './api';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 const API_KEY = require("./secrets");
-const URL = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=" + API_KEY;
+const URL = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=" + API_KEY;
 
 export default class Forecast extends React.Component
 {
@@ -35,29 +36,40 @@ export default class Forecast extends React.Component
         if(this.state.forecast.weather == null){
             return(<p>Loading...</p>);
         }
-        return (
-            <div>
-                <h1>{ this.state.forecast.name }</h1>
-                { this.state.forecast.weather.map(weather =>
-                    <ul>
-                        <li>ID: {weather.id}</li>
-                        <li>Forecast: {weather.main}</li>
-                        <li>Description: {weather.description}</li>
-                        <li>Description: {weather.icon}</li>
-                    </ul>
-                ) }
 
-                {
-                    <ul>
-                        <li>Feels like: {this.state.forecast.main.feels_like}</li>
-                        <li>Temperature: {this.state.forecast.main.temp}</li>
-                        <li>Max: {this.state.forecast.main.temp_max}</li>
-                        <li>Min: {this.state.forecast.main.temp_min}</li>
-                    </ul>
-                }
-                <p>ICON: {this.state.forecast.weather[0].icon}</p>
-                <img src={ "http://openweathermap.org/img/wn/" + this.state.forecast.weather[0].icon + "@2x.png" } />
-            </div>
+        const container = {
+            backgroundColor: "#007ea8",
+            "background-image": "linear-gradient(#0091f2, #004b91)",
+            width: "20%",
+            padding: "10px",
+            "margin-top": "100px",
+            fontFamily: "Avenir",
+            "border-radius": "30px",
+            border: "2px solid #ffffff",
+            color: "white"
+        };
+
+        return (
+            <Container style={container}>
+                <Row className="justify-content-md-center">
+                    <Col><img src={ "http://openweathermap.org/img/wn/" + this.state.forecast.weather[0].icon + "@2x.png" } /></Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col><h1 style={{color: "#00376b"}}>{this.state.forecast.name}</h1></Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col>Currently {this.state.forecast.weather[0].main}</Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col md="auto" style={{fontSize: "60px"}}>{parseInt(this.state.forecast.main.temp)}° C</Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col>Max:{this.state.forecast.main.temp_max}° Min:{this.state.forecast.main.temp_min}°</Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col>Feels like {this.state.forecast.main.feels_like}°</Col>
+                </Row>
+            </Container>
         )
     }
 }
